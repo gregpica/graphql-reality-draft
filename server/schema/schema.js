3ce5d6fd5867fa2da6ffd43d4d305f4e75bcca32
gregpica <gpica@bu.edu>
2019-02-20 11:52:27 -0800

    Create DrafterType and drafter root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,6 +16,15 @@ const ShowType = new GraphQLObjectType({
     })
 })
 
+const DrafterType = new GraphQLObjectType({
+    name: 'Drafter',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        image: {type: GraphQLString}
+    })
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -25,6 +34,17 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 const showQuery = `SELECT * FROM "shows" WHERE id=${args.id}`;
                 return db.one(showQuery)
+                    .then(data => {
+                        return data;
+                    })
+            }
+        },
+        drafter: {
+            type: DrafterType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                const drafterQuery = `SELECT * FROM "drafters" WHERE id=${args.id}`;
+                return db.one(drafterQuery)
                     .then(data => {
                         return data;
                     })
