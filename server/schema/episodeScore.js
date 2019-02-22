7f874e171d29f8789a3d1c9ff8af576423fa15e3
gregpica <gpica@bu.edu>
2019-02-21 16:34:37 -0800

    Create addEpisodeScore mutation

--- server/schema/episodeScore.js
+++ server/schema/episodeScore.js
@@ -1,4 +1,10 @@
+const db = require('./psqlAdapter');
+
 const typeDef = `
+    extend type Mutation {
+        addEpisodeScore(showId: ID!, characterId: ID!, number: Int!, points: Int!): EpisodeScore
+    }
+
     type EpisodeScore {
         id: ID!
         number: Int!
@@ -6,6 +12,19 @@ const typeDef = `
     }
 `;
 
+const resolvers = {
+    Mutation: {
+        addEpisodeScore: (parent, args) => {
+            const addEpisodeScoreQuery = `INSERT INTO episode_scores("showId", "characterId", number, points) VALUES ('${args.showId}', '${args.characterId}', '${args.number}', '${args.points}') RETURNING id, number, points`;
+            return db.one(addEpisodeScoreQuery)
+                .then(data => {
+                    return data 
+                })
+        }
+    }
+};
+
 module.exports = {
-    EpisodeScore: typeDef
+    EpisodeScore: typeDef,
+    episodeScoreResolvers: resolvers
 };
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@ const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
 const { Show, showResolvers } = require('./show.js');
 const { Drafter, drafterResolvers } = require('./drafter.js');
 const { Character, characterResolvers } = require('./character.js');
-const { EpisodeScore } = require('./episodeScore.js');
+const { EpisodeScore, episodeScoreResolvers } = require('./episodeScore.js');
 const { Rule, ruleResolvers } = require('./rule.js')
 
 const Query = `
@@ -19,7 +19,7 @@ const Mutation = `
  
 module.exports = makeExecutableSchema({
     typeDefs: [Query, Mutation, Show, Drafter, Character, EpisodeScore, Rule],
-    resolvers: [showResolvers, drafterResolvers, characterResolvers, ruleResolvers]
+    resolvers: [showResolvers, drafterResolvers, characterResolvers, episodeScoreResolvers, ruleResolvers]
 });
 
 
